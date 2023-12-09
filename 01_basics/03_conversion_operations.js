a7c84f930c08f0695fe7cbdf668457842ae34d72
Atharva Ahirekar <100533115+AtharvaAhirekar@users.noreply.github.com>
2023-12-09 07:12:29 +0000

    Conversions

---
+++ 01_basics/03_conversion_operations.js
@@ -0,0 +1,28 @@
+let score = 33 ;
+let score1 = "99" ;
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// console.log(typeof score1);
+// console.log(typeof(score1));
+
+
+let numscore = Number(score1);
+// console.log(typeof numscore)
+// console.log(numscore);
+
+let gamename = "GTA5"
+// console.log(gamename);
+// console.log(typeof gamename);
+
+let gamenumname  = Number(gamename);
+// console.log(gamenumname);
+// console.log(typeof(gamenumname))
+
+let isloggedin  ;
+// console.log(typeof isloggedin)
+
+
+let isboolloggedin = Boolean(isloggedin);
+// console.log(isboolloggedin);
+// console.log(typeof (isboolloggedin));
