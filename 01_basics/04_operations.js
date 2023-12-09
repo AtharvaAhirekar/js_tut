f33d9ed6607647b857f42cb85203f20d973db0e5
Atharva Ahirekar <100533115+AtharvaAhirekar@users.noreply.github.com>
2023-12-09 09:54:38 +0000

    Arithmetic_operators_Comparisons

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,25 @@
+/************************************************ Operations *******************************************************/
+console.log("Jay Shree Ram");
+
+let num1 = 5 ;
+let num2 = 1 ;
+
+// console.log(num1+num2);
+// console.log(num1-num2);
+// console.log(num1*num2);
+// console.log(num1/num2);
+// console.log(num1 % num2);
+// console.log(num1**2);
+
+let str1 = "Hello "
+let str2 = "World"
+// console.log(str1+str2);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+2+1);
+// console.log(2+2+"2");
+
+let gameCounter = 5 ;
+console.log(gameCounter++);
