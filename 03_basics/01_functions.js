1f70be4e377fd028a2bdde83dbc476bdb17881d4
Atharva Ahirekar <100533115+AtharvaAhirekar@users.noreply.github.com>
2023-12-12 13:25:55 +0000

    Functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,54 @@
+function sums (num1 , num2){
+    return (num1+num2)
+}
+
+// sums(1 , 2) ;
+// sums(1 , "4") ;
+// sums("1" , 4) ;
+// sums(1 , "a");
+// sums(1 , null);
+
+// let result = sums(4,2);
+// console.log(result);
+
+// function isloggedin(username = "default"){
+
+//     if(!username){
+//         return `username is not given` ;
+//     }
+//     return `Hello , user ${username} just logged in` ;
+// } 
+
+// console.log(isloggedin())
+
+
+// Rest Operator and Spread Operator === ........
+
+// function calculatecartprice(...item){
+//     return item ;
+// }
+
+// console.log(calculatecartprice(200 , 400 , 700))
+
+
+/*Function and Object*/
+// const user1 = {
+//     name : " user123",
+//     price : 201
+
+// }
+
+// function handleobject(object) {
+//     return `Hello ${object.name} , your pending money is ${object.price}`
+// }
+// console.log(handleobject(user1))
+
+/* Function and Array*/
+
+// const mynewarray = [1,2,3,4,5,6];
+
+// function getarrayelement(myarray){
+//     return myarray[2];
+// }
+
+// console.log(getarrayelement(mynewarray))
