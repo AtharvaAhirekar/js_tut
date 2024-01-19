ed0e3034d3f5f9bf5839305c9423652806588241
Atharva Ahirekar <100533115+AtharvaAhirekar@users.noreply.github.com>
2024-01-19 06:50:42 +0000

    Classes

--- 09_Classes_and_Oops/myclasses.js
+++ 09_Classes_and_Oops/myclasses.js
@@ -1,18 +1,37 @@
-class User {
-    constructor(username , email , password ){
-        this.username = username 
-        this.email = email 
-        this.password = password
-    }
+// class User {
+//     constructor(username , email , password ){
+//         this.username = username 
+//         this.email = email 
+//         this.password = password
+//     }
 
-    encryptPassword(){
-        return `${this.password}abc`
-    }
-    getallcaps(){
-        return`${this.username.toUpperCase()}`
-    }
+//     encryptPassword(){
+//         return `${this.password}abc`
+//     }
+//     getallcaps(){
+//         return`${this.username.toUpperCase()}`
+//     }
+// }
+
+// const atharva = new User("atharva" , "atharva@gmail.com" , 123)
+// console.log(atharva.encryptPassword());
+// console.log(atharva.getallcaps());
+
+// Traditional Method 
+
+function employee(clientname , email , password){
+    this.clientname = clientname
+    this.email = email 
+    this.password = password
 }
 
-const atharva = new User("atharva" , "atharva@gmail.com" , 123)
-console.log(atharva.encryptPassword());
-console.log(atharva.getallcaps());
+employee.prototype.getencryptPassword = function(){
+    return(`${this.password}`);
+}
+employee.prototype.getallcaps = function(){
+    return(`${this.clientname}`);
+}
+
+const emp1 = new employee('Karan','karan@google.com',123)
+console.log(emp1.getencryptPassword());
+console.log(emp1.getallcaps());
